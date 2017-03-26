46c374cd4a20c0eb97519c80db0fde39d66a5a94
Cliff <cliffsmith25@gmail.com>
2017-03-25 20:23:42 -0500

    Rebuilt after altering webpack.base.conf.js file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import 'font-awesome-webpack'
 import 'vue-awesome/icons'
 
 Vue.config.productionTip = false
